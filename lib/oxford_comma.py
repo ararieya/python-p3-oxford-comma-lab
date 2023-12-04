9db8de66d3884865898c957c5eb000994e41c154
Ashley Rarieya <ararieya@gmail.com>
2023-12-04 14:35:32 +0300

    Completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    item_string = ""
+    if len(items) == 1:
+        item_string = "".join(items)
+        return item_string
+    elif len(items) == 2:
+        item_string = " and ".join(items)
+        return item_string
+    else:
+        item_string = ", ".join(items[:-1])
+        oxford_item_string = f"{item_string}, and {items[-1]}"
+        return oxford_item_string
+
+print(oxford_comma(["air", "water", "fire"]))
